68fc257c1dc5d240e9ded4fac6754643dea4fe22
Manuel Frigerio <manuel@friger.io>
2025-08-02 21:30:20 +0200

    endpoint

--- lib/convertkit/client/subscribers.rb
+++ lib/convertkit/client/subscribers.rb
@@ -13,6 +13,29 @@ module Convertkit
         connection.get("subscribers/#{subscriber_id}/tags")
       end
 
+      # returns an hash with the following keys:
+      # {
+      #   "subscriber": {
+      #     "id": 3436990966,
+      #     "stats": {
+      #       "sent": 2,
+      #       "opened": 2,
+      #       "clicked": 2,
+      #       "bounced": 0,
+      #       "open_rate": 1,
+      #       "click_rate": 1,
+      #       "last_sent": "2025-06-25 16:44:37.000",
+      #       "last_opened": "2025-06-25 16:45:36.000",
+      #       "last_clicked": "2025-06-25 16:45:42.000",
+      #       "sent_since_last_open": 0,
+      #       "sent_since_last_click": 0
+      #     }
+      #   }
+      # }
+      def subscriber_stats(subscriber_id)
+        connection.get("subscribers/#{subscriber_id}/stats")
+      end
+
       def update_subscriber(subscriber_id, options = {})
         response = connection.put("subscribers/#{subscriber_id}") do |f|
           f.params["email_address"] = options[:email_address] if options[:email_address]
